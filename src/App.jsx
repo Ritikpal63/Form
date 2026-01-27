50c34e681bd5afa664b82fa547a5f183cb021e4d
Ritik <ritikpal227@gmail.com>
2026-01-27 22:34:39 +0530

    Add Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { Routes, Route, Navigate } from 'react-router'
 import Login from './components/Login'
 import Signup from './components/Signup'
 import Dashboard from './components/Dashboard'
+import EmployeeData from './components/EmployeeData'
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Navigate to="/signin" replace />} />
+        <Route path='/' element={<EmployeeData />} />
+        {/* <Route path='/' element={<Navigate to="/signin" replace />} /> */}
         <Route path='/signin' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/dashboard' element={<Dashboard onLogout={() => window.location.href = '/signin'} />} />
       </Routes>
+      
     </>
   )
 }
