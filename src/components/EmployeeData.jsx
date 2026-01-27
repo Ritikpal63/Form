079f8cd4cf8d101e0771ddf1d44670551cfb8a84
Ritik <ritikpal227@gmail.com>
2026-01-27 22:56:01 +0530

    Update

--- src/components/EmployeeData.jsx
+++ src/components/EmployeeData.jsx
@@ -4,19 +4,19 @@ const EmployeeData = () => {
   const data = JSON.parse(localStorage.getItem('user')) || []
   return (
     <>
-    <table className='table'>
-      <thead>
+    <table className='border-collapse border border-gray-400'>
+      <thead className=''>
         <tr>
-          <td>Name</td>
-          <td>Email</td>
+          <th className='border border-gray-300 p-3'>Name</th>
+          <th className='border border-gray-300 p-3'>Email</th>
         </tr>
       </thead>
       <tbody>
       {
     data.map((item)=>{
       return <tr key={item.email}>
-        <td>{item.name}</td>
-        <td>{item.email}</td>
+        <td className='border border-gray-300 p-3'>{item.name}</td>
+        <td className='border border-gray-300 p-3'>{item.email}</td>
       </tr>
     })
     }
